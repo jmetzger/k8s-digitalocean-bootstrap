7507480fce409f70ef6ab23287f3415837ad387c
Jochen Metzger <t3sec.jm@googlemail.com>
2025-06-09 19:50:06 +0000

    Edit outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,9 @@
 output "ingress_domain" {
   value = "auto.do.t3isp.de"
 }
+
+output "kubeconfig" {
+  value     = digitalocean_kubernetes_cluster.main.kube_configs[0].raw_config
+  sensitive = true
+}
+
