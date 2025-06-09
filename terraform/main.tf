56e9452360b9e0360cebb598b237ec40c36b160c
Jochen Metzger <t3sec.jm@googlemail.com>
2025-06-09 19:13:19 +0000

    Edit main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,7 +16,7 @@ provider "digitalocean" {
 resource "digitalocean_kubernetes_cluster" "main" {
   name    = "gitlab-cluster"
   region  = "fra1"
-  version = "1.29.1-do.0"
+  version = "latest"
 
   node_pool {
     name       = "default-pool"
